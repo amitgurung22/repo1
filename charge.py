a834a90abba4c20f2b792b52d07dc5d3063947d2
Amit Gurung <amitgurung22@gmail.com>
2025-07-01 10:34:58 -0500

    gs read

--- charge.py
+++ charge.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
+from streamlit_gsheets import GSheetsConnection
 
 if "spots" not in st.session_state:
   st.session_state.spots = 4
@@ -45,6 +46,17 @@ st.divider()
 for i in range(10):
     st.write(f"{i}:user{i}")
 st.divider()
+
+# Create a connection object.
+conn = st.connection("gsheets", type=GSheetsConnection)
+
+df = conn.read()
+
+df 
+
+# Print results.
+#for row in df.itertuples():
+#    st.write(f"{row.name} has a :{row.pet}:")
 
 
     
