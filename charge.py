d21bbe28539f197d1f316c92c606efd7b178a35e
Amit Gurung <amitgurung22@gmail.com>
2025-07-09 13:01:03 +0000

    updates

--- charge.py
+++ charge.py
@@ -1,33 +1,54 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-import requests
+import gspread
+from google.oauth2.service_account import Credentials
+
 import subprocess
 
 #from streamlit_gsheets import GSheetsConnection
+#@st.cache_data
+def get_sheet_data():
+   scopes = ["https://www.googleapis.com/auth/spreadsheets"]
+   creds = Credentials.from_service_account_file("credentials.json", scopes=scopes)
+   client = gspread.authorize(creds)
+   sheet_id = "1X5-Q9X9Z2IzR604BIefQfr1iRS7qmMPGbFLgBH5LBW8"
+   sh = client.open_by_key(sheet_id)
+   ws = []
+   for i in range(len(sh.worksheets())):
+     ws.append(sh.get_worksheet(i))
+   return ws
 
-if "spots" not in st.session_state:
-  st.session_state.spots = 4
-st.write(f"Number of spots available : {st.session_state.spots}")
-st.divider()
+spots = 0
+ws = get_sheet_data()
 
+print("***** debug1 *****")
+spots = int(ws[0].cell(1,1).value)
+print(f"***** debug2 : {spots} *****")
+st.session_state['spots'] = spots
+st.write(f"Number of spots available : {spots}")
 
 #spots = 0
 def restore_spots():
     st.write("restoring spots...")
-    st.session_state.spots = 4
+    ws[0].update([[4]],'A1')
     
 def take_spot():
-    if st.session_state.spots :
-      st.session_state.spots = st.session_state.spots -1 
+    if st.session_state['spots'] > 0:
+      st.session_state['spots'] = st.session_state['spots'] - 1
+      #spots = spots -1 
+      #ws[0].update([[spots]],'A1')
+      ws[0].update([[st.session_state['spots']]],'A1')
       st.write("taking a spot ...")
+      st.rerun()
     else:
         st.write("No spots available")    
 
 def release_spot():
-    if (st.session_state.spots < 4):
+    if (spots < 4):
       st.write("Releasing a spot...") 
-      st.session_state.spots = st.session_state.spots +1
+      spots = spots +1 
+      ws[0].update([[spots]],'A1')
     
     
 t = st.button("take a spot")
